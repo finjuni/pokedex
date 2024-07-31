32e2db976d1f06eae167ab936c465dbb56a3c33f
delino1win <delino1win@gmail.com>
2024-07-31 16:13:39 +0700

    minor fix list pokemons

--- pokedex/src/components/list/listPokemon.jsx
+++ pokedex/src/components/list/listPokemon.jsx
@@ -1,21 +1,26 @@
-
-import PokemonCard from '../card/pokemonCard'
+import PokemonCard from "../card/pokemonCard";
 
 export default function ListPokemon(props) {
-
   // let loopPokeCard = []
 
   // for(let i = 0; i < 12; i++) {
   //  loopPokeCard.push(i)
   // }
 
-  if(!props) return []
+  if (!props) return [];
+
+  const listPokemon = props.props;
+
+  console.log("listPoke: ", listPokemon);
+
 
   return (
-    <div className='w-full h-full max-sm:justify-center flex flex-wrap gap-7'>
-      {props.map(item => (
-        <PokemonCard key={item} item={item}/>
+    <div className="w-full h-full max-sm:justify-center flex flex-wrap gap-7">
+      {listPokemon.map((item) => (
+    
+          <PokemonCard key={item.id} item={item} />
+        
       ))}
     </div>
-  )
+  );
 }
