c883c62e02b2d61727e00b919f03091b257b02c7
delino1win <delino1win@gmail.com>
2024-08-01 00:52:10 +0700

    create server and create bookmark button

--- pokedex/src/pages/detail/pokemonDetail.jsx
+++ pokedex/src/pages/detail/pokemonDetail.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getDetailPokemon } from "../../fetch/fetch"
-
+import Swal from 'sweetalert2'
+import axios from "axios"
 
 export default function PokemonDetail() {
 
@@ -20,12 +21,55 @@ export default function PokemonDetail() {
     fetchData()
   }, [])
 
+  const onBookmarked = async (event) => {
+    event.preventDefault()
+
+    // const task = {
+    //   id: randomId(),
+    //   desc: inputTask,
+    //   category: category,
+    //   isDone: false,
+    //   createdAt: currentDate()
+    // }
+
+    if(!detailPokemon) return 
+
+    const pokemon = detailPokemon
+
+    try {
+      
+      // await axios.post('https://skillful-crawling-idea.glitch.me/tasks', task)
+      // await Swal.fire({
+      //   title: "To Do Added",
+      //   icon: "success"
+      // })
+
+      await axios.post(`http://localhost:3000/bookmarks`, pokemon)
+
+      await Swal.fire({
+          title: "To Do Added",
+          icon: "success"
+        })
+
+    } catch (error) {
+      console.error(error)
+      await Swal.fire({
+        title: `${error.message}`,
+        icon: "error"
+      })
+    }
+  }
+
   return (
-    <div className="">
+    <div className="flex flex-col h-full ">
       <label>poke name: {detailPokemon.name}</label>
       <label>poke id: {detailPokemon.id}</label>
-      <div>weight: {detailPokemon.weight}</div>
+      <label>weight: {detailPokemon.weight}</label>
       <div>sex</div>
+
+      <form onSubmit={onBookmarked}>
+        <button type="submit" className="bg-slate-300 p-2">add</button>
+      </form>
     </div>
   )
 }
